c50e416fc1844a1ac9102d372ebcf5e178b1f187
HolidayGuy21 <14449@coderacademy.edu.au>
2023-06-25 12:23:22 +1000

    added get books,  register and login routes

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy import Column, Integer, String, Float
 import os
 import psycopg2
@@ -8,9 +11,13 @@ app = Flask(__name__)
 
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "postgresql+psycopg2://DonQuixote:Sancho@localhost:5432/bookclub"
+] = "postgresql+psycopg2://ben:amigo@localhost:5432/bookclub"
+app.config['JWT_SECRET_KEY'] = 'friend'
+
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
+jwt = JWTManager(app)
 
 @app.cli.command('db_create')
 def db_create(): 
@@ -57,34 +64,44 @@ def db_seed():
     db.session.commit()
     print('Database seeded successfully')
 
-@app.route('/')
-def hello_world():
-    return 'ayup world'
-
-@app.route('/supersimple')
-def supersimple():
-    return jsonify(message='supersimple supersimple'), 200
-
-@app.route('/notfound')
-def notfound():
-    return jsonify(message='not found blud'), 404
-
-@app.route('/parameters')
-def parameters():
-    name = request.args.get('name')
-    age = int(request.args.get('age'))
-    if age < 18:
-        return jsonify(message="sorry ur not old enough"), 401 
-    else:
-        return jsonify(message="welcome u good blud"), 400
-
-@app.route('/URLvariables/<string:name>/<int:age>')
-def URLvariables(name: str, age: int):
-    if age < 18:
-        return jsonify(message="sorry ur not old enough"), 401 
-    else:
-        return jsonify(message="welcome u good blud"), 400
     
+@app.route("/books", methods=["GET"])
+def books():
+    books_list = Book.query.all()
+    result = books_schema.dump(books_list)
+    return jsonify(result)
+
+@app.route("/register", methods=['POST'])
+def register():
+    email = request.form['email']
+    test = User.query.filter_by(email=email).first()
+    if test: 
+        return jsonify(message="that email already exists"), 409
+    else:
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        user = User(username=username, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify(message="User created successfully"), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.is_json:
+        email = request.json['email']
+        password = request.json['password']
+    else:
+        email = request.form['email']
+        password = request.form['password']
+
+    test = User.query.filter_by(email=email, password=password).first()
+    if test:
+        access_token = create_access_token(identity=email)
+        return jsonify(message="Login successful!", access_token=access_token)
+    else:
+        return jsonify(message="Email or password incorrect"), 401
+
     # database models
 class User(db.Model):
     __tablename__ = "users"
@@ -105,8 +122,20 @@ class Book(db.Model):
     synopsis = db.Column(db.String, nullable=False)
     publication_year = db.Column(db.Integer, nullable=False)
 
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'username', 'email', 'password', 'is_admin')
+
+class BookSchema(ma.Schema):
+    class Meta:
+        fields = ('book_id', 'title', 'author', 'genre', 'synopsis', 'publication_year')
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+book_schema = BookSchema()
+books_schema = BookSchema(many=True)
 
 
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
